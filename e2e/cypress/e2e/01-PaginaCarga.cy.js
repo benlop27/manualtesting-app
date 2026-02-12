114d82696d33d5fc3af0db2bdcad99c224e9594d
Benjamin Lopez <benlop27@icloud.com>
2026-02-11 20:27:17 -0600

    feat: agregar cambios en cypress

--- e2e/cypress/e2e/01-PaginaCarga.cy.js
+++ e2e/cypress/e2e/01-PaginaCarga.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('https://manualtesting-dev-138959494334.us-central1.run.app/')
 
     cy.contains('¡Bienvenido a nuestra aplicación web!')
     
