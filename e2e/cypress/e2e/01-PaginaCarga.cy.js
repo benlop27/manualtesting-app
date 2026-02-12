29a255695b2565fa341da460278cb9a63ffbdb21
Benjamin Lopez <benlop27@icloud.com>
2026-02-11 20:44:11 -0600

    feat: agregadas un par de pruebas e2e

--- e2e/cypress/e2e/01-PaginaCarga.cy.js
+++ e2e/cypress/e2e/01-PaginaCarga.cy.js
@@ -1,8 +1,22 @@
-describe('01-PaginaPrincipal-PaginaCargaConMensaje', () => {
-  it('passes', () => {
+describe('Suite de pruebas: Página Principal', () => {
+  before(() => {
+    // Cargar la página una sola vez antes de todos los tests
     cy.visit('https://manualtesting-dev-138959494334.us-central1.run.app/')
-
-    cy.contains('¡Bienvenido a la suite de usuarios!')
-    
   })
-})
+
+  it('Página carga con mensaje de bienvenida', () => {
+    cy.contains('¡Bienvenido a la suite de usuarios!')
+  })
+
+  it('Página no muestra errores', () => {
+    cy.get('body').should('not.contain', 'Error')
+  })
+
+  it('Página muestra tabla', () => {
+    cy.get('table').should('exist')
+  })
+
+  it('Tabla tiene datos', () => {
+    cy.get('table tbody tr').its('length').should('be.gte', 1)
+  })
+})  
