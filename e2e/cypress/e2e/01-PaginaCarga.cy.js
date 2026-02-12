950ca516bc27479ad4ebd5f966665d8a16a8024f
Benjamin Lopez <benlop27@icloud.com>
2026-02-11 20:34:38 -0600

    feat: arreglada la prueba

--- e2e/cypress/e2e/01-PaginaCarga.cy.js
+++ e2e/cypress/e2e/01-PaginaCarga.cy.js
@@ -1,8 +1,8 @@
-describe('template spec', () => {
+describe('01-PaginaPrincipal-PaginaCargaConMensaje', () => {
   it('passes', () => {
     cy.visit('https://manualtesting-dev-138959494334.us-central1.run.app/')
 
-    cy.contains('¡Bienvenido a nuestra aplicación web!')
+    cy.contains('¡Bienvenido a la suite de usuarios!')
     
   })
 })
